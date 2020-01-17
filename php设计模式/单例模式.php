3873118920c45506c37e9d996bd7193e3e93cf80
renyafei <18401223106@163.com>
2020-01-17 11:28:20 +0800

    tijiao

---
+++ php设计模式/单例模式.php
@@ -0,0 +1,26 @@
+<?php
+
+class Test
+{
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }
+}
