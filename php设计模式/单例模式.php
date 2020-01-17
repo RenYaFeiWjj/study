29ebbed8b23256fbef4121766285d90aec6f6410
renyafei <18401223106@163.com>
2020-01-17 11:34:09 +0800

    ‘单例’控制住基类，在源头上限制这个类，使其无法生成多个对象，如果已经生成过，直接返回

--- php设计模式/单例模式.php
+++ php设计模式/单例模式.php
@@ -3,24 +3,47 @@
 class Test
 {
     private static $instance = null;
+    private $name = '';
 
+    /**
+     * @return Test|null
+     * 任亚飞
+     * 单例模式指只需要实例化一次
+     * 场景如: 配置类、session类、Cache类、File类、Database类
+     */
     public static function getInstance()
     {
+        //控制住基类，在源头上限制这个类，使其无法生成多个对象，如果已经生成过，直接返回
         if (self::$instance == null) {
             self::$instance = new self();
         }
         return self::$instance;
     }
 
+    public function getName()
+    {
+        return $this->name = "张三";
+    }
+
     private function __construct()
     {
     }
 
+    /**
+     * 任亚飞
+     * 不允许克隆
+     */
     private function __clone()
     {
     }
 
+    /**
+     * 任亚飞
+     * 不允许重建
+     */
     private function __wakeup()
     {
     }
 }
+
+echo Test::getInstance()->getName();
